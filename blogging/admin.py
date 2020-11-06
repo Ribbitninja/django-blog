a1578d070a4aa444c15177080f66f69c44962c3e
Ribbitninja <60951520+Ribbitninja@users.noreply.github.com>
2020-11-05 21:46:30 -0800

    inline category commit.

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+# Define an inline class here
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
