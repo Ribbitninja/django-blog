d7bdbe12fada94685df14b21134c5a73e8cea5a9
Ribbitninja <60951520+Ribbitninja@users.noreply.github.com>
2020-11-16 21:26:08 -0800

    class-based blogging commit

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from blogging.views import stub_view
-from blogging.views import list_view
-from blogging.views import detail_view
+# from blogging.views import list_view
+# from blogging.views import detail_view
+from django.urls import path
+from blogging.views import BlogListView, BlogDetailView
 
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', BlogListView.as_view(), name="blog_index"),
+    path("blog/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
